ed27f91951f01d1c5cd7d64a0d494adb5542fa9d
Aayush14-design <aayushlimbachiya14@gmail.com>
2026-09-16 21:58:31 +0530

    Create RearrangeArray.java
    Rearrange Array Elements by Sign — Brute & Optimal.
    Brute: O(3n/2) or O(n) with extra arrays | Optimal: O(n), O(n) space

---
+++ Arrays/RearrangeArray.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Main18{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        // data entry loop
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println("Enter the value of index "+i);
+            arr[i]= sc.nextInt();
+        }
+        // display elements
+        display(arr);
+        //Rearrange the elements
+        RearrangeArray obj=new RearrangeArray();
+        int ans[]=obj.rearrangeArray(arr);
+        //display after arranging
+        System.out.println("After Arranging ");
+        display(ans);
+    }
+    static   void display(int arr[]){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
+public class RearrangeArray {
+    int[] rearrangeArray(int arr[]){
+        int posIndex=0;
+        int negIndex=1;
+        int ans[]=new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>0){
+                ans[posIndex]=arr[i];
+                posIndex+=2;
+            }
+            else {
+                ans[negIndex]=arr[i];
+                negIndex+=2;
+            }
+        }
+        return ans;
+    }
+}
